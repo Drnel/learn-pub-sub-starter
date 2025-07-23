e29fc1c31cc5e606422c99ac258c1bf8d5d46e13
Drnel <mahesh121995@gmail.com>
2025-07-23 02:18:56 +0000

    added basic publish fuctionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,11 +15,27 @@ func main() {
 	connection_strign := "amqp://guest:guest@localhost:5672/"
 	connection, err := amqp.Dial(connection_strign)
 	if err != nil {
-		fmt.Println("Error connection: ", err)
+		fmt.Println("Error getting connection: ", err)
 		return
 	}
 	defer connection.Close()
 	fmt.Println("Connection Sucessful...")
+
+	channel, err := connection.Channel()
+	if err != nil {
+		fmt.Println("Error creating channel:", err)
+		return
+	}
+	err = pubsub.PublishJSON(
+		channel, routing.ExchangePerilDirect, routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		fmt.Println("Error publishing:", err)
+		return
+	}
+	fmt.Println("Sucessfully Published")
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt)
 	sig := <-sigs
